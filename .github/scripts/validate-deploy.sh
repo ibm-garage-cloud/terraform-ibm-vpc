6c8a7f6bf13e428cc675d4f8f812f5ea37d96358
Sean Sundberg <seansund@us.ibm.com>
2021-03-24 22:49:40 -0500

    Updates vpc name in test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 echo "terraform.tfvars"
 cat terraform.tfvars
 
-PREFIX_NAME=$(cat terraform.tfvars | grep name_prefix | sed "s/name_prefix=//g" | sed 's/"//g')
+PREFIX_NAME=$(cat terraform.tfvars | grep name_prefix | sed "s/name_prefix=//g" | sed 's/"//g' | sed "s/_/-/g")
 PUBLIC_GATEWAY=$(cat terraform.tfvars | grep vpc_public_gateway | sed "s/vpc_public_gateway=//g" | sed 's/"//g')
 REGION=$(cat terraform.tfvars | grep -E "^region" | sed "s/region=//g" | sed 's/"//g')
 RESOURCE_GROUP_NAME=$(cat terraform.tfvars | grep resource_group_name | sed "s/resource_group_name=//g" | sed 's/"//g')

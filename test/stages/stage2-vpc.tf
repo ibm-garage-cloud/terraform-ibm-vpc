22e5f13ae68901e797728548c53298033a546e0d
Sean Sundberg <seansund@us.ibm.com>
2021-04-12 15:51:26 -0500

    Splits subnet and gateway logic out of vpc module
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-vpc.tf
+++ test/stages/stage2-vpc.tf
@@ -5,7 +5,4 @@ module "dev_vpc" {
   region              = var.region
   name_prefix         = var.name_prefix
   ibmcloud_api_key    = var.ibmcloud_api_key
-  subnet_count        = var.vpc_subnet_count
-  subnets             = jsondecode(var.vpc_subnets)
-  public_gateway      = var.vpc_public_gateway == "true"
 }

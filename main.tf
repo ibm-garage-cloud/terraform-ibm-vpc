c41ff7c1824fecef04486805f61f1ece416353fc
Sean Sundberg <seansund@us.ibm.com>
2021-04-12 23:26:36 -0500

    Moves generic security group rules back here
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ locals {
   prefix_name       = var.name_prefix != "" ? var.name_prefix : var.resource_group_name
   vpc_name          = lower(replace(var.name != "" ? var.name : "${local.prefix_name}-vpc", "_", "-"))
   vpc_id            = ibm_is_vpc.vpc.id
+  security_group_id = ibm_is_vpc.vpc.default_security_group
 }
 
 resource ibm_is_vpc vpc {
@@ -29,5 +30,55 @@ resource ibm_is_network_acl network_acl {
     source      = "0.0.0.0/0"
     destination = "0.0.0.0/0"
     direction   = "inbound"
+  }
+}
+
+resource ibm_is_security_group_rule rule_icmp_ping {
+  group     = local.security_group_id
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  icmp {
+    type = 8
+  }
+}
+
+# from https://cloud.ibm.com/docs/vpc?topic=vpc-service-endpoints-for-vpc
+resource ibm_is_security_group_rule "cse_dns_1" {
+  group     = local.security_group_id
+  direction = "outbound"
+  remote    = "161.26.0.10"
+  udp {
+    port_min = 53
+    port_max = 53
+  }
+}
+
+resource ibm_is_security_group_rule cse_dns_2 {
+  group     = local.security_group_id
+  direction = "outbound"
+  remote    = "161.26.0.11"
+  udp {
+    port_min = 53
+    port_max = 53
+  }
+}
+
+resource ibm_is_security_group_rule private_dns_1 {
+  group     = local.security_group_id
+  direction = "outbound"
+  remote    = "161.26.0.7"
+  udp {
+    port_min = 53
+    port_max = 53
+  }
+}
+
+resource ibm_is_security_group_rule private_dns_2 {
+  group     = local.security_group_id
+  direction = "outbound"
+  remote    = "161.26.0.8"
+  udp {
+    port_min = 53
+    port_max = 53
   }
 }
